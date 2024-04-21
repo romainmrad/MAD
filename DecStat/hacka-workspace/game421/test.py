0b7fc785f3fd4b5908fbada174d85b5c664345f3
Romain Mrad <romain.mrad@gmail.com>
2024-04-21 18:26:57 +0200

    Removing unused files

--- DecStat/hacka-workspace/game421/test.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open("policy-WELCOME.json", "r") as file:
-    data = json.load(file)
-
-with open('res.txt', "w") as file:
-    for key, value in data.items():
-        file.write(f'\tpolicy.put("{key}", "{value}")\n')
